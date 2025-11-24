e3e33b38e43edbeb9a9e978d7ca9d88f4e457fc0
ales-fava <alessandro.fava@hotmail.com>
2025-11-24 17:12:04 +0100

    feat: add release flag

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,28 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TaskForm } from './components/task-form.component';
-import { TaskList } from './components/task-list.component';
-import { TaskService } from './services/task.service';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, TaskForm, TaskList],
-  templateUrl: './app.html',
+  template: `
+    <h1>Mi Web App DevOps</h1>
+    @if(showNewWelcome){
+      <div style="background-color: yellow; padding: 20px;">
+        🚀 ¡NUEVA FUNCIONALIDAD ACTIVADA DESDE GITOPS! 🚀
+      </div>
+    }`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly taskService = inject(TaskService);
+  showNewWelcome = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Carga la configuración en tiempo de ejecución
+    this.http.get<any>('/assets/config.json').subscribe(config => {
+      this.showNewWelcome = config.showNewWelcome;
+    });
+  }
+  // protected readonly taskService = inject(TaskService);
 }
